d7e2956ae0fbe8bbfb49e38ecca82ec76a60c391
Morgan Muell <morganrmuell@gmail.com>
2021-02-24 17:35:26 -0500

    Added a docstring under the functionget_smallest_prime_number in order to explain the function and make it more informative and helpful when using hte help function within python3.

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python3
 
-# A script for getting the smallest prime factor of an integer.
-
-# IT'S NOT WORKING ANYMORE EVEN THOUGH I ONLY DID THE FIRST STEP IAWEHOAI
+"A module for getting the smallest prime factor of an integer."
 
 import sys
 
 
 def get_smallest_prime_factor(n):
+    """
+    Returns the smallest integer that is a factor of 'n'.
+
+    If 'n' is a prime number 'None' is returned.
+
+    Parameters
+    ----------
+    n: int
+        The integer to be factored.
+
+    Returns
+    -------
+    int or None
+        The smallest integer that is a factor of 'n'
+        or None if 'n' is a prime.
+    """
+
     for i in range(2, n):
         if (n % i) == 0:
             return i
