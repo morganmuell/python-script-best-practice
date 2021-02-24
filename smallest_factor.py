d47422dc650772f2cb59b252b898fe9b4ec02c1f
Morgan Muell <morganrmuell@gmail.com>
2021-02-24 17:11:30 -0500

    Added function to define for get smallest prime factor

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,17 +4,17 @@
 
 import sys
 
+def get_smallest_prime_factor(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            smallest_prime_factor = i
+    return None
+
 if len(sys.argv) != 2:
     sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
 n = int(sys.argv[1])
 if n < 1:
     sys.exit(sys.argv[0] + ": Expecting a positive integer")
-
-smallest_prime_factor = None
-for i in range(2, n):
-    if (n % i) == 0:
-        smallest_prime_factor = i
-        break
 
 if smallest_prime_factor is None:
     print(n)
